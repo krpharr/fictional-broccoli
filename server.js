6545e2088d2e35e501a4e3e06dc4e6f777e48982
rpharr <rpharr@richmond.edu>
2020-02-03 23:07:01 -0500

    add department

--- server.js
+++ server.js
@@ -5,74 +5,31 @@ const validator = require("validator");
 
 let connection;
 
-function login() {
-    const questions = [{
-            type: 'input',
-            name: 'username',
-            message: 'username:',
-            validate: function(value) {
-                var valid = !validator.isEmpty(value);
-                return valid || 'username can not be empty.';
-            }
-        },
-        {
-            type: 'password',
-            message: 'password:',
-            name: 'password',
-            mask: '*',
-            validate: function(value) {
-                var valid = !validator.isEmpty(value);
-                return valid || 'password can not be empty.';
-            }
-        }
-    ];
-    inquirer.prompt(questions).then(answers => {
-        console.log(answers.username, answers.password);
-        let { username: user, password } = answers;
-        connectUser(user, password);
-    });
-};
+connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "9EBu6swH",
+    database: 'cms_db'
+});
 
-function connectUser(user, password) {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: user,
-        password: password,
-    });
-    connection.connect(function(err) {
-        if (err) throw err;
-        console.log("connected as id " + connection.threadId + "\n");
-        if (databaseExists()) {
-            //inquireModifyDatabase();
-            start();
-        } else {
-            buildDatabase();
-        }
-    });
-};
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
 
-function databaseExists() {
-    //return boolean if project database already exists in mysql
-    connection.changeUser({ database: 'cms_db' }, function(err) {
-        if (err) return false;
-    });
-
-};
-
-function buildDatabase() {
-
-};
 
 function start() {
     let msg = `
     ADD - add departments, roles, and employees.
     VIEW - view departments, roles, employees, employees by manager, and utilized budget by department.
     UPDATE - update employee roles and employee managers.`;
+    console.log(msg);
     const questions = [{
         type: 'list',
         name: 'action',
-        message: msg,
+        message: 'Select option',
         choices: [
             'ADD',
             'VIEW',
@@ -94,12 +51,14 @@ function start() {
     });
 };
 
+////////////////////////////////////////////////////////////
 function add() {
     let msg = `ADD - departments, roles, and employees.`;
+    console.log(msg);
     const questions = [{
         type: 'list',
         name: 'action',
-        message: msg,
+        message: 'Select option',
         choices: [
             'Department',
             'Role',
@@ -125,12 +84,64 @@ function add() {
     });
 };
 
+function selectAllFromTable(table, cb) {
+
+    connection.query(
+        `SELECT * FROM ${table}`,
+        function(err, res) {
+            if (err) throw err;
+            let tb = cTable.getTable(res);
+            console.log(tb);
+            cb();
+        }
+    );
+
+};
+
+function addDepartment() {
+    selectAllFromTable("department", inquireDepartment);
+};
+
+function inquireDepartment() {
+    const questions = [{
+        type: 'input',
+        name: 'name',
+        message: 'Enter name for new department.',
+        validate: function(value) {
+            var valid = !validator.isEmpty(value);
+            return valid || 'Department name can not be empty.';
+        }
+    }];
+    inquirer.prompt(questions).then(answers => {
+        console.log(answers.name);
+        connection.query(
+            "INSERT INTO department (name) VALUES (?)", [answers.name],
+            function(err, res) {
+                if (err) throw err;
+                console.log(`${answers.name} sucessfully added to database.`);
+                add();
+            }
+        );
+    });
+};
+
+function addRole() {
+
+};
+
+function addEmployee() {
+
+};
+
+//////////////////////////////////////////////////////////
+
 function view() {
     let msg = `VIEW - departments, roles, employees, employees by manager, and utilized budget by department.`;
+    console.log(msg);
     const questions = [{
         type: 'list',
         name: 'action',
-        message: msg,
+        message: 'Select option',
         choices: [
             'Department',
             'Role',
@@ -164,10 +175,143 @@ function view() {
     });
 };
 
+function viewDepartment() {
+
+};
+
+function viewRole() {
+
+};
+
+function viewEmployee() {
+
+};
+
+function viewEmployeesByManager() {
+
+};
+
+function viewUtilizedBudgetByDepartment() {
+
+};
+
+///////////////////////////////////////////
+
+function update() {
+    let msg = `UPDATE - employee roles and employee managers.`;
+    const questions = [{
+        type: 'list',
+        name: 'action',
+        message: msg,
+        choices: [
+            'Employee role',
+            'Employee manager',
+            '<-Back'
+        ],
+    }];
+    inquirer.prompt(questions).then(answers => {
+        switch (answers.action) {
+            case 'Employee role':
+                updateEmployeeRole();
+                break;
+            case 'Employee manager':
+                updateEmployeeManager();
+                break;
+            case '<-Back':
+                start();
+                break;
+        }
+    });
+};
+
+function updateEmployeeRole() {
+
+};
+
+function updateEmployeeManager() {
+
+};
+
+///////////////////////////////////////////////////////////////////////
+// function login() {
+//     const questions = [{
+//             type: 'input',
+//             name: 'username',
+//             message: 'username:',
+//             validate: function(value) {
+//                 var valid = !validator.isEmpty(value);
+//                 return valid || 'username can not be empty.';
+//             }
+//         },
+//         {
+//             type: 'password',
+//             message: 'password:',
+//             name: 'password',
+//             mask: '*',
+//             validate: function(value) {
+//                 var valid = !validator.isEmpty(value);
+//                 return valid || 'password can not be empty.';
+//             }
+//         }
+//     ];
+//     inquirer.prompt(questions).then(answers => {
+//         console.log(answers.username, answers.password);
+//         let { username: user, password } = answers;
+//         connectUser(user, password);
+//     });
+// };
+
+// function connectUser(user, password) {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: user,
+//         password: password,
+//     });
+//     connection.connect(function(err) {
+//         if (err) throw err;
+//         console.log("connected as id " + connection.threadId + "\n");
+//         // if (databaseExists()) {
+//         //     //inquireModifyDatabase();
+//         //     start();
+//         // } else {
+//         //     buildDatabase();
+//         // }
+//         databaseExists().then(res => {
+//             console.log(res);
+//         });
+//     });
+// };
+
+// async function databaseExists() {
+//     try {
+//         let connected;
+//         connected = await connection.changeUser({ database: 'cms_db' }, function(err) {
+//             // if (err) return false;
+//             // else return true;
+//         });
+//         return connected;
+//     } catch (err) {
+
+//     }
+//     //return boolean if project database already exists in mysql
+//     // let connected = false;
+//     // connection.changeUser({ database: 'cms_db' }, function(err) {
+//     //     if (err) connected = false;
+//     //     else connected = true;
+//     // });
+//     // return connected;
+
+// };
+
+// function buildDatabase() {
+
+// };
+
 ///////////////////////////////////////////////////////////////////////
 
 
-login();
+// login();
 
 // connection.changeUser({ database: 'my_database' }, function(err) {
 //     if (err) throw err;
